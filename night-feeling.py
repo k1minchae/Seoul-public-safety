2bfb13e2d12faaeee502839d830006811207538c
pfeeling <jf_feeling@naver.com>
2025-04-15 20:10:47 +0900

    feeling

---
+++ night-feeling.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/adult_entertainment.csv', encoding='cp949')
+
+df.head()
+df.info()
+
+
+
+
