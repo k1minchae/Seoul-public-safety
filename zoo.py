7c36a5f21430a18753d11b56742f815c1bb2ccdc
twoweeksbro <twoweeksbro@gmail.com>
2025-04-16 20:31:57 +0900

    주형

--- zoo.py
+++ zoo.py
@@ -21,6 +21,11 @@ cctv['자치구'].value_counts()
 
 # 서울 안전벨 수
 bell = pd.read_excel('./data/Seoul_Safetybell.xlsx')
+bell['자치구'] = bell['관리기관명'].str.extract(r'([가-힣]+구)')
+bell['자치구'].value_counts()
+bell['자치구'] = bell['자치구'].str.replace('서울시성북구','성북구')
+bell.to_excel('./data/Seoul_Safetybell.xlsx', index=False)
+
 
 bell.info()         # 21091
 bell.head()
@@ -40,6 +45,44 @@ bell['구2'].isna().sum()       # 8392
 
 bell['자치구'] = bell['구1'].fillna(bell['구2'])
 bell.to_excel('./data/Seoul_Safetybell.xlsx', index=False)
+
+# 시각화 시작
+
+import plotly.express as px
+
+import json
+with open('./data/seoul_districts.geojson', encoding='utf-8') as f:
+    geojson_data = json.load(f)
+
+
+agg_df = (bell.groupby("자치구",
+                         as_index=False)
+                         .count())
+
+agg_df = agg_df.rename(columns={"자치구": "SIG_KOR_NM"})
+print(agg_df.head(2))
+
+
+fig = px.choropleth_mapbox(
+    agg_df,
+    geojson=geojson_data,
+    locations="SIG_KOR_NM",
+    featureidkey="properties.SIG_KOR_NM",
+    color="LCD합계",
+    color_continuous_scale="Blues",
+    mapbox_style="carto-positron",
+    center={"lat": 37.5665, "lon": 126.9780},
+    zoom=10,
+    opacity=0.7,
+    title="서울시 자치구별 LCD 거치대 수"
+    )
+
+fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+fig.show()
+
+
+
+
 
 
 
